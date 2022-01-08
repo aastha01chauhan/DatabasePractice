cfe5918425c72cf5eb1afb48dfb8f896702e0080
Aastha Chauhan <aastha.chauhan01@gmail.com>
2022-01-08 22:20:27 +0530

    initial commit

---
+++ app/src/androidTest/java/Word.kt
@@ -0,0 +1,14 @@
+package org.openlake.databasepractice
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "word_table")
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "word") val word: String
+
+)
+
+
---
+++ app/src/androidTest/java/WordsApplication.kt.kt
@@ -0,0 +1,8 @@
+package org.openlake.databasepractice
+
+class `WordsApplication.kt` : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
